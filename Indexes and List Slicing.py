e9a1f2ee2ae827c1e7317e5aa2e24c6c19766be0
Muhammad Zain <112485536+muhammadzain001@users.noreply.github.com>
2022-10-25 20:34:50 +0300

    Add files via upload

---
+++ Indexes and List Slicing.py
@@ -0,0 +1,11 @@
+# Accessing by Indexes
+# Indexes start at 0
+
+index_emp = ["carpet", "hardwood", "linoleum"]
+print(index_emp[1])
+
+var5 = [[1, 2, 3], [4,5], [6, 7, 8, 9]]
+print(var5[1][0])
+
+index_emp = ["carpet", "hardwood", "linoleum"]
+del index_emp[0]
